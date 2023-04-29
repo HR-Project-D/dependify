144dc94e79ee841cb7d80449f4a2e7afb5594b59
Lucas Prins <lucas2002prins@gmail.com>
2023-04-29 16:17:18 +0200

    Added plugin to add all tailwind colors as global css variables

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +18,15 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), addVariablesForColors],
+}
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+      ":root": newVars,
+  });
 }
