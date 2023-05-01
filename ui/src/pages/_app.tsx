ac54996499929fe1b6e2c30ff26f46ea3bd5006f
Lucas Prins <lucas2002prins@gmail.com>
2023-05-01 20:37:58 +0200

    Fixed minor css inconsistencies on the command menu

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const [userState, userDispatch] = useReducer(userReducer, initialUserState);
 
   return (
-    <ThemeProvider attribute='class' disableTransitionOnChange defaultTheme='system'>
+    <ThemeProvider attribute='class' disableTransitionOnChange defaultTheme='dark'>
       <UserContext.Provider value={{ state: userState, dispatch: userDispatch }}>
         <Component {...pageProps} />
       </UserContext.Provider>
