a3b824f9b6765559b3836e9da0ab90d0537cb7bc
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2023-05-20 17:36:03 +0200

    bash script update

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 python3 manage.py runserver 0.0.0.0:8000 &
 cd ./ui
-npm run start
+npm run start&
+if [[ ! -e "./setup_key" ]]; then
+    echo "$(openssl rand -hex 16)" | sudo tee ./setup_key
+    cat ./setup_key
+fi
