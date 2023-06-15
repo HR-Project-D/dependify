8bde550aaf3d34ab4e36044e2501234955148f7f
lucasprins <lucas2002prins@gmail.com>
2023-06-15 11:46:55 +0200

    Added data source creation to the front-end

---
+++ ui/src/types/api/api-data-source.d.ts
@@ -0,0 +1,9 @@
+export type APIResponseGenerateDataSource = {
+  message?: string;
+  error?: string;
+  public_key: string;
+};
+
+export type APIResponseConfirmDataSource = {
+  message: string;
+};
