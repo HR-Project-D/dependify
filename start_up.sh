33a1184a2cb6e6059a3ae1674230a0c66e5b4e27
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2023-06-19 15:43:07 +0200

    bugfixes

--- start_up.sh
+++ start_up.sh
@@ -4,7 +4,9 @@ if [[ ! -e "./data/setup_key" ]]; then
     cd data
     echo "Generating setup key"
     echo -n "$(openssl rand -hex 16)" | tee ./setup_key
+    echo ""
     cd ../
 fi
+python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000 &
 node server.js
